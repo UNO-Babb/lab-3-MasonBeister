b6a534406c4d7e4847ea40201e184732d30b9f6d
MasonBeister <mbeister@unomaha.edu>
2025-02-05 16:12:47 +0000

    Done with RPS

--- RPS.py
+++ RPS.py
@@ -10,6 +10,50 @@ def main():
   losses = 0
   #Create a loop that continues as long as the user wants to play.
   #User can play as many games as they wish.
+  play = "Y"
+  while play == "Y":
+
+    player = input("Enter choice (R), (P), (S): ")
+
+    computer = random.choice(["R", "P", "S"])
+
+    if player == "R":
+      print("Player chose Rock")
+    elif player == "P":
+      print("Player chose Paper")
+    elif player == "S":
+      print("Player chose Scissors")
+    else:
+      print("Invalid Option")
+
+    if player == computer: 
+      print("TIE")
+      ties = ties + 1
+    elif player == "R" and computer == "S":
+      print("You Win!")
+      wins = wins + 1
+    elif player == "R" and computer == "P":
+      print("You Lose.")
+      losses = losses + 1
+    elif player == "P" and computer == "R":
+      print("You Win!")
+      wins = wins + 1
+    elif player == "P" and computer == "S":
+      print("You Lose.")
+      losses = losses + 1
+    elif player == "S" and computer == ("P"):
+      print("You Win!")
+      wins = wins + 1
+    elif player == "S" and computer == ("R"):
+      print("You Lose.")
+      losses = losses + 1
+
+ 
+ 
+    play = input("Play again? (Y/N): ")
+  play = "N"
+  print("End of game")
+
 
   #Randomly choose the computer between 'R', 'P', or 'S'
   #Prompt the user for their RPS selection
